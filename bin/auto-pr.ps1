902d919b5c02afdb9413b274e2e19ec0f54b7c52
Diogo Marques <diogo.marques@ALIDATA.PT>
2024-08-12 15:42:26 +0100

    stuff

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,9 +1,11 @@
 param(
     # overwrite upstream param
     [String]$upstream = "alidata-diogo-marques/scoop-alidata:main"
+    # overwrite originbranch param
+    [String]$originbranch = "main"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
 $autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
 $dir = "$PSScriptRoot/../bucket" # checks the parent dir
-Invoke-Expression -command "& '$autopr' -dir '$dir' -upstream $upstream $($args | ForEach-Object { "$_ " })"
+Invoke-Expression -command "& '$autopr' -dir '$dir' -upstream $upstream -originbranch $originbranch $($args | ForEach-Object { "$_ " })"
